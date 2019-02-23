e3d26a4a312fdbe952f8dcebb3ca1a34c68f3b07
anabelmaresc <ana.belisa9403@gmail.com>
2019-02-23 17:35:54 -0500

    Add function to processComand

--- stock/app/Http/Controllers/ProductController.php
+++ stock/app/Http/Controllers/ProductController.php
@@ -8,21 +8,35 @@ class ProductController extends Controller
 {
     //This funtion will read a testingCsv on develop mode [WIP]
     public function readCsv(){ //For the moment, we are'nt receiving any request
-        //We have a temporally .csv file to testing on develop mode
-        $file_path = public_path('temporal/testing.csv'); //File path
+        $file_path = public_path('temporal/testing.csv'); //We have a temporally .csv file to testing on develop mode
         $file = fopen($file_path, "r"); //Open the file
-        $data = fgetcsv($file, 1000, ","); //Get the file content to read here line per line
-        $row = 1;
-        while ($data !== FALSE) {
-            $num = count($data); //Count of fields in each line
-            echo "<p> $num fields in line $row: <br /></p>\n";
+        $row = 0;
+        while (($data = fgetcsv($file, ",")) == true) {
+            $num = count($data); //Count of fields (or commands) in each line
             $row++;
-
-            //For each field
-            for ($i=0; $i<$num; $i++) {
-                echo $data[$i] . "<br />\n"; //Each field
+            for ($i=0; $i<$num; $i++) { 
+                $idproduct = $data[0];
+                $command = $data[1];
+                $units = isset($data[2]) ? $data[2] : null;
             }
+            $this->processCommand($idproduct, $command, $units);
         }
         fclose($file);
+    }
+
+    private function processCommand($idproduct, $command, $units){
+        switch($command){
+            case 'Agregar':
+            break;
+            case 'Restar':
+            break;
+            case 'Activar':
+            break;
+            case 'Desactivar':
+            break;
+            default:
+                return 'No command found';
+            break;
+        }
     }
 }
