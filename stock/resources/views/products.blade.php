4eda432cc0ad4da93e4c56a4c9c7d1944d310311
anabelmaresc <ana.belisa9403@gmail.com>
2019-02-23 19:19:27 -0500

    Create products.blade.php and table structure html

---
+++ stock/resources/views/products.blade.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Stock</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+</head>
+<body>
+    <header>
+        <h1>Stock</h1>
+    </header>
+    <main>
+        <table>
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Name</th>
+                    <th>Reference</th>
+                    <th>Price</th>
+                    <th>Cost</th>
+                    <th>Units</th>
+                    <th>State</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+    </main>
+</body>
+</html>
